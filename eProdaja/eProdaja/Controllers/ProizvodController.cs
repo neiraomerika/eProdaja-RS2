8542d2bb95965697af78d16f1499c249ce0397c6
Neira Omerika <neira.omerika@edu.fit.ba>
2021-08-12 14:00:17 +0200

    V1 -Git repo,basic Rest API,Dependency injection

---
+++ eProdaja/eProdaja/Controllers/ProizvodController.cs
@@ -0,0 +1,53 @@
+﻿using eProdaja.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eProdaja.Controllers
+{
+    [ApiController]
+    [Microsoft.AspNetCore.Mvc.Route( "[controller]")]
+    public class ProizvodController : ControllerBase
+    {
+        public IProizvodService _proizvodService { get; set; }
+
+        public ProizvodController(IProizvodService proizvodService)
+        {
+            _proizvodService = proizvodService;
+        }
+
+       
+
+        [HttpGet]
+        public IEnumerable<Proizvod> Get()
+        {
+         
+            return _proizvodService.Get();
+        }
+        [HttpGet("{id}")]
+        public Proizvod GetById(int id)
+        {
+            return _proizvodService.GetById(id);
+        }
+        [HttpPost]
+        public Proizvod Insert(Proizvod proizvod)
+        {
+            return _proizvodService.Insert(proizvod);
+        }
+        [HttpPut("{id}")]
+        public Proizvod Update(int id, Proizvod proizvod)
+        {
+            return _proizvodService.Update(id, proizvod);
+        }
+    }
+    public class Proizvod
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
